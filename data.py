849dedbf6e449c1380ebf3c1e187123d52015564
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-05-21 13:13:59 -0400

    initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+PITCHERS = [
+  { "id": 1, "firstName": "Ranger", "lastName": "Suarez", "team": 1},
+  { "id": 2, "firstName": "Aaron", "lastName": "Nola", "team": 1},
+  { "id": 3, "firstName": "Shane", "lastName": "Baz", "team": 2},
+  { "id": 4, "firstName": "Luis", "lastName": "Patino", "team": 2},
+]
+
+TEAMS = [
+  { "id": 1, "name": "Philadelphia Phillies"},
+  { id: 2, "name": "Tampa Bay Rays"},
+]
+
+GAMES = [
+  { "id": 1, "homeTeam": 1, "awayTeam": 2, "date": "2022-05-01", "startingPitchers": [1, 3]},
+  { "id": 1, "homeTeam": 1, "awayTeam": 2, "date": "2022-05-02", "startingPitchers": [2, 4]},
+  { "id": 1, "homeTeam": 2, "awayTeam": 1, "date": "2022-05-15", "startingPitchers": [2, 3]},
+]
