b47ba2d89eeb3e55865480c5ea839ba20be10164
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-05-21 13:18:00 -0400

    reformat data

--- data.py
+++ data.py
@@ -1,17 +1,17 @@
 PITCHERS = [
-  { "id": 1, "firstName": "Ranger", "lastName": "Suarez", "team": 1},
-  { "id": 2, "firstName": "Aaron", "lastName": "Nola", "team": 1},
-  { "id": 3, "firstName": "Shane", "lastName": "Baz", "team": 2},
-  { "id": 4, "firstName": "Luis", "lastName": "Patino", "team": 2},
+  { "id": 1, "first_name": "Ranger", "last_name": "Suarez", "team": 1},
+  { "id": 2, "first_name": "Aaron", "last_name": "Nola", "team": 1},
+  { "id": 3, "first_name": "Shane", "last_name": "Baz", "team": 2},
+  { "id": 4, "first_name": "Luis", "last_name": "Patino", "team": 2},
 ]
 
 TEAMS = [
   { "id": 1, "name": "Philadelphia Phillies"},
-  { id: 2, "name": "Tampa Bay Rays"},
+  { "id": 2, "name": "Tampa Bay Rays"},
 ]
 
 GAMES = [
-  { "id": 1, "homeTeam": 1, "awayTeam": 2, "date": "2022-05-01", "startingPitchers": [1, 3]},
-  { "id": 1, "homeTeam": 1, "awayTeam": 2, "date": "2022-05-02", "startingPitchers": [2, 4]},
-  { "id": 1, "homeTeam": 2, "awayTeam": 1, "date": "2022-05-15", "startingPitchers": [2, 3]},
+  { "id": 1, "home_team": 1, "away_team": 2, "date": "2022-05-01", "starting_pitchers": [1, 3]},
+  { "id": 2, "home_team": 1, "away_team": 2, "date": "2022-05-02", "starting_pitchers": [2, 4]},
+  { "id": 3, "home_team": 2, "away_team": 1, "date": "2022-05-15", "starting_pitchers": [2, 3]},
 ]
